33d5f82bdfaab1ce2b57f5dae98a406d575785f2
Jimmy Andreas <jimmysignature@gmail.com>
2022-03-31 20:52:39 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array) {
+    
+    const result = array.find(element => element.result === "W")
+     
+    if (result){
+        return result.year
+    }
+}
